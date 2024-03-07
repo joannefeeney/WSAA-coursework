3c0a7fe9ef6194abf503ba43f24bc78830047220
Joanne <G00283773@atu.ie>
2024-03-07 21:12:31 +0000

    Starting second assignment

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,10 @@
+# assignment03-cso.py
+# By Joanne Feeney
+# In this assignment I will Write a program that retrieves the dataset for the 
+# "exchequer account (historical series)" from the CSO, and store it into a file called "cso.json"
+
+# Importing packages
+import requests
+
+# Setting URL
+url = 
